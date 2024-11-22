253c62008feca81261ae1c0983b95acb579764c8
Chandler <chandler.j.ward@gmail.com>
2024-11-22 13:11:34 -0700

    Initial commit - practice code

---
+++ index.js
@@ -0,0 +1,52 @@
+import { HfInference } from '@huggingface/inference'
+
+// Create your Hugging Face Token: https://huggingface.co/settings/tokens
+// Set your Hugging Face Token: https://scrimba.com/dashboard#env
+// Learn more: https://scrimba.com/links/env-variables
+const hf = new HfInference(import.meta.env.VITE_HF_TOKEN)
+
+// Hugging Face Inference API docs: https://huggingface.co/docs/huggingface.js/inference/README
+
+
+// TEXT GENERATION
+
+// const textToGenerate = "The definition of machine learning inference is "
+
+// const response = await hf.textGeneration({
+//     inputs: textToGenerate,
+//     model: "HuggingFaceH4/zephyr-7b-beta"
+// })
+
+// console.log(response)
+// console.log('Token exists:', !!import.meta.env.VITE_HF_TOKEN)
+
+
+// Text Classification - Sentiment Analysis
+
+// const textToClassify = "I just bought a new camera. It's been awesome."
+
+// const response = await hf.textClassification({
+//     model: "SamLowe/roberta-base-go_emotions",
+//     inputs: textToClassify
+// })
+
+// console.log(response[0].label)
+// console.log(response)
+
+
+// Text Translation
+
+const textToTranslate = "It's an exciting time to be an AI engineer"
+
+const textTranslationResponse = await hf.translation({
+    model: 'facebook/mbart-large-50-many-to-many-mmt',
+    inputs: textToTranslate,
+    parameters: {
+        src_lang: "en_XX",
+        tgt_lang: "es_XX"
+    }
+  })
+  
+  const translation = textTranslationResponse.translation_text
+  console.log("\ntranslation:\n")
+  console.log(translation)
